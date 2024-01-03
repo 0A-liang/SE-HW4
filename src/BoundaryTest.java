0c9947d4cc04f11e83ff1128bbdd5ef1508cc132
亮節 鍾 <t112598014@ntut.edu.tw>
2024-01-03 23:37:33 +0800

    Coverage 100%

--- src/BoundaryTest.java
+++ src/BoundaryTest.java
@@ -1,2 +1,48 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class BoundaryTest {
+
+    @Test
+    public void Boundary0Test() {
+        Assertions.assertEquals('X', LetterGrade.letterGrade(-1));
+        Assertions.assertEquals('F', LetterGrade.letterGrade(0));
+        Assertions.assertEquals('F', LetterGrade.letterGrade(1));
+    }
+
+    @Test
+    public void Boundary60Test() {
+        Assertions.assertEquals('F', LetterGrade.letterGrade(59));
+        Assertions.assertEquals('D', LetterGrade.letterGrade(60));
+        Assertions.assertEquals('D', LetterGrade.letterGrade(61));
+    }
+
+    @Test
+    public void Boundary70Test() {
+        Assertions.assertEquals('D', LetterGrade.letterGrade(69));
+        Assertions.assertEquals('C', LetterGrade.letterGrade(70));
+        Assertions.assertEquals('C', LetterGrade.letterGrade(71));
+    }
+
+    @Test
+    public void Boundary80Test() {
+        Assertions.assertEquals('C', LetterGrade.letterGrade(79));
+        Assertions.assertEquals('B', LetterGrade.letterGrade(80));
+        Assertions.assertEquals('B', LetterGrade.letterGrade(81));
+    }
+
+    @Test
+    public void Boundary90Test() {
+        Assertions.assertEquals('B', LetterGrade.letterGrade(89));
+        Assertions.assertEquals('A', LetterGrade.letterGrade(90));
+        Assertions.assertEquals('A', LetterGrade.letterGrade(91));
+    }
+
+    @Test
+    public void Boundary100Test() {
+        Assertions.assertEquals('A', LetterGrade.letterGrade(99));
+        Assertions.assertEquals('A', LetterGrade.letterGrade(100));
+        Assertions.assertEquals('X', LetterGrade.letterGrade(101));
+    }
+
 }
--- src/PartitionTest.java
+++ src/PartitionTest.java
@@ -1,2 +1,41 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class PartitionTest {
+
+    @Test
+    public void Between90To100() {
+        Assertions.assertEquals('A', LetterGrade.letterGrade(94));
+    }
+
+    @Test
+    public void Between80To89() {
+        Assertions.assertEquals('B', LetterGrade.letterGrade(83));
+    }
+
+    @Test
+    public void Between70To79() {
+        Assertions.assertEquals('C',LetterGrade.letterGrade(75));
+    }
+
+    @Test
+    public void Between60To69() {
+        Assertions.assertEquals('D', LetterGrade.letterGrade(67));
+    }
+
+    @Test
+    public void Between0To59() {
+        Assertions.assertEquals('F', LetterGrade.letterGrade(36));
+    }
+
+    @Test
+    public void GraterThan100() {
+        Assertions.assertEquals('X', LetterGrade.letterGrade(115));
+    }
+
+    @Test
+    public void LessThan0() {
+        Assertions.assertEquals('X', LetterGrade.letterGrade(-54));
+    }
+
 }
